9c37aa73f3545932722dfb7e287e22b69bbe4a55
Aldi12221 <ifdalaldi@gmail.com>
2025-11-27 17:18:14 +0700

    3

--- belajar/script.js
+++ belajar/script.js
@@ -1,7 +1,11 @@
-const WIDTH =10;
-const HEIGHT =10;
+const WIDTH =12;
+const HEIGHT =12;
 
 let map=[];
+let player ={
+    x:1,
+    y:1
+}
 
 for(let y=0;y<HEIGHT;y++
 ){
@@ -9,7 +13,7 @@ for(let y=0;y<HEIGHT;y++
 
     for(let x=0;x<WIDTH;x++){
         
-        if(y===0 || y===HEIGHT-1 || x=== 0||x===WIDTH-1){
+        if((y === 0 || y === HEIGHT-1 || x=== 0||x===WIDTH-1 )||( y===5 && x === 5)){
             map[y][x]=1;
         }else{
             map[y][x]=0
@@ -22,12 +26,15 @@ function render (){
     const gameElement = document.getElementById('game')
     gameElement.innerHTML = '';
 
-    for(let y=0;y<HEIGHT;y++){
-        for(let x=0;x<WIDTH;x++){
+    for (let y = 0; y <HEIGHT; y++){
+        for (let x = 0;x<WIDTH; x++){
             const cell = document.createElement('div');
             cell.classList.add('cell');
             if (map[y][x] === 1) {
                 cell.classList.add("wall"); 
+            }
+            if(player.x === x && player.y === y){
+                cell.style.background='yellow'
             }
             
             
@@ -36,4 +43,24 @@ function render (){
         }
 
 }
-render();
+document.addEventListener('keydown',(e)=>{
+    let nx = player.x
+    let ny = player.y
+
+    if(e.key === 'w'||e.key=='ArrowUp')ny--;
+    if(e.key==='s'||e.key==='ArrowDown')ny++;
+    if(e.key==='a'|| e.key==='ArrowLeft')nx--;
+    if(e.key==='d')nx++;
+
+    if(map[ny][nx]===0){
+        player.x = nx;
+        player.y = ny;
+        
+    }else{
+        alert('ada tembok');
+    }
+
+    render();
+
+})
+ render();
---
+++ belajar/test.py
@@ -0,0 +1,8 @@
+soal = 'berapa kaki ayam'
+jawaban = '4'
+print(soal)
+user_input = input('masukkan jawaban: ')
+if user_input == jawaban:
+    print('benar')
+else: return print('salah')
+
